f49c6ea31890655dcab376495c44c07251fa293a
Rahul Gupta <rahulelex8@gmail.com>
2022-04-26 09:34:26 +0530

    -Modified to make script user interactive --Added to change net device dynamiclly in setting up an environment

--- install_ros.sh
+++ install_ros.sh
@@ -1,8 +1,4 @@
 #! /bin/sh
-#
-#
-#
-
 Code=$(cat /etc/lsb-release | grep -oP "^DISTRIB_CODENAME=\K.*")
 remove_ros() {
     sudo dpkg --configure -a
@@ -21,8 +17,11 @@ install_ros() {
     sudo apt install ros-$1-desktop-full -y
 }
 setup_env() {
+    nmcli -t -f DEVICE,TYPE device | cut -f1 -d":"
+    echo "Enter your device type"
+    read device_type
     echo "source /opt/ros/$1/setup.bash" >> ~/.bashrc
-    echo "value="$(ip a s wlp2s0 | egrep -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2)"" >> ~/.bashrc
+    echo "value="$(ip a s $device_type | egrep -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2)"" >> ~/.bashrc
     echo "export ROS_MASTER_URI=http://$value:11311" >> ~/.bashrc
     echo "export ROS_HOSTNAME=$value" >> ~/.bashrc
     echo "export ROS_IP=$value" >> ~/.bashrc
@@ -34,16 +33,40 @@ ros_deps() {
     sudo rosdep init
     rosdep update
 }
+script() {
+while :
+	do 
+		echo $1 $2
+	    echo "Please select operation to perform"
+	    echo "1. Remove already installed ROS"
+	    echo "2. Install ROS for $1"
+	    echo "3. Setup environment"
+	    echo "4. Exit"
+	    read operation
+	    case $operation in
+			"1")
+				remove_ros
+			;;
+			"2")
+				install_ros $1
+				;;
+			"3")
+				setup_env $1
+				ros_deps $2
+				sudo apt-get autoremove -y
+				echo "REBOOT your system"
+			;;
+			"4")
+				break
+			;;
+		esac
+	done
+}
 case  $1  in
     "melodic")
     if [ "$Code" = "bionic" ] || [ "$Code" = "artful" ]
     then
-        remove_ros
-        install_ros melodic
-        setup_env melodic
-        ros_deps python
-        sudo apt-get autoremove -y
-        echo "REBOOT your system"
+		script melodic python
     else
         echo "Ubuntu $Code does not support ROS melodic"
     fi
@@ -51,11 +74,7 @@ case  $1  in
     "neotic")
         if [ "$Code" = "focal" ]
         then
-            remove_ros
-            install_ros neotic
-            setup_env neotic
-            ros_deps python3
-            sudo apt-get autoremove -y
+            script neotic python3
             echo "REBOOT your system"
         else
             echo "Ubuntu $Code does not support ROS Noetic"
